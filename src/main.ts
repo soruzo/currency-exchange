c2a1808fff33625fc34cb50534343e84b58129cd
bruno moura <dev.zbnmoura@gmail.com>
2023-04-16 13:35:32 -0300

    feat: adding gereric exception rule

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(port, '0.0.0.0');
 }
 
