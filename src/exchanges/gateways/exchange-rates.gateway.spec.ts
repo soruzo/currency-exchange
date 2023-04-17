fb622ed927c9be1a6526870a8d17f434acd939cc
bruno moura <dev.zbnmoura@gmail.com>
2023-04-17 14:58:57 -0300

    test: adding gateway test

--- src/exchanges/gateways/exchange-rates.gateway.spec.ts
+++ src/exchanges/gateways/exchange-rates.gateway.spec.ts
@@ -1,20 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExchangeRatesGateway } from './exchange-rates.gateway';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
 
 describe('ExchangeRatesGateway', () => {
-  let gateway: ExchangeRatesGateway;
+  let exchangeRatesGateway: ExchangeRatesGateway;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [ExchangeRatesGateway],
     }).compile();
 
-    gateway = module.get<ExchangeRatesGateway>(ExchangeRatesGateway);
+    exchangeRatesGateway = module.get<ExchangeRatesGateway>(ExchangeRatesGateway);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
-    expect(gateway).toBeDefined();
+    expect(exchangeRatesGateway).toBeDefined();
   });
 
-  // Adicione seus testes específicos aqui
+  describe('convert', () => {
+    it('should return converted amount', async () => {
+      const amount = 100;
+      const from = 'USD';
+      const to = 'EUR';
+      const expectedResult = {
+        success: true,
+        query: {
+          from: "EUR",
+          to: "BRL",
+          amount: 1
+        },
+        info: {
+          timestamp: 1681728963,
+          quote: 5.394539
+        },
+        result: 5.394539
+      }
+
+      const mockResponse = {
+        data: expectedResult,
+      };
+
+      httpService.get = jest.fn().mockImplementation(() => of(mockResponse));
+
+      const result = await exchangeRatesGateway.convert(amount, from, to);
+
+      expect(httpService.get).toBeCalledWith(
+        expect.stringMatching(/convert/),
+        expect.objectContaining({
+          params: { amount, from, to },
+          headers: {
+            apikey: expect.any(String),
+          },
+        }),
+      );
+      expect(result).toEqual(expectedResult);
+    });
+  });
 });
