382733044008d4495e7b387ce9935b14d10034b1
bruno moura <dev.zbnmoura@gmail.com>
2023-04-17 09:06:07 -0300

    test: implementing unit test

--- src/exchanges/controllers/exchanges.controller.spec.ts
+++ src/exchanges/controllers/exchanges.controller.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExchangesController } from './exchanges.controller';
+import { ExchangesService } from '../services/exchanges.service';
+import { CreateExchangeRequestDto } from '../dto/create-exchange-request.dto';
+import { ExchangeResponseDto } from '../dto/exchange-response.dto';
 
 describe('ExchangesController', () => {
   let controller: ExchangesController;
+  let service: ExchangesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExchangesController],
+      providers: [
+        {
+          provide: ExchangesService,
+          useValue: {
+            findByUserId: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ExchangesController>(ExchangesController);
+    service = module.get<ExchangesService>(ExchangesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAllById', () => {
+    it('should call ExchangesService.findByUserId with correct parameter', async () => {
+      const userId = 'user-id';
+      await controller.findByUserId(userId);
+      expect(service.findByUserId).toHaveBeenCalledWith(userId);
+    });
+
+    it('should return the result of ExchangesService.findByUserId', async () => {
+      const userId = 'user-id';
+      const expectedResult: ExchangeResponseDto[] = [];
+      (service.findByUserId as jest.Mock).mockResolvedValue(expectedResult);
+      const result = await controller.findByUserId(userId);
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('create', () => {
+    it('should call ExchangesService.create with correct parameter', async () => {
+      const createExchangeDto: CreateExchangeRequestDto = {
+        userId: 'user-id',
+        sourceCurrency: 'USD',
+        sourceValue: 100,
+        targetCurrency: 'EUR',
+      };
+      await controller.create(createExchangeDto);
+      expect(service.create).toHaveBeenCalledWith(createExchangeDto);
+    });
+
+    it('should return the result of ExchangesService.create', async () => {
+      const createExchangeDto: CreateExchangeRequestDto = {
+        userId: 'user-id',
+        sourceCurrency: 'USD',
+        sourceValue: 100,
+        targetCurrency: 'EUR',
+      };
+      const expectedResult: ExchangeResponseDto = {
+        transactionId: 'exchange-id',
+        rate: 1,
+        targetValue: 2,
+        datetime: "",
+        ...createExchangeDto,
+      };
+      (service.create as jest.Mock).mockResolvedValue(expectedResult);
+      const result = await controller.create(createExchangeDto);
+      expect(result).toEqual(expectedResult);
+    });
   });
 });
--- src/exchanges/repositories/exchange.repository.spec.ts
+++ src/exchanges/repositories/exchange.repository.spec.ts
@@ -1,20 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExchangeRepository } from './exchange.repository';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Exchange } from '../entities/exchange.entity';
 
 describe('ExchangeRepository', () => {
-  let repository: ExchangeRepository;
+  let exchangeRepository: ExchangeRepository;
+  const mockExchangeRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeRepository],
+      providers: [
+        ExchangeRepository,
+        {
+          provide: getRepositoryToken(Exchange),
+          useValue: mockExchangeRepository,
+        },
+      ],
     }).compile();
 
-    repository = module.get<ExchangeRepository>(ExchangeRepository);
+    exchangeRepository = module.get<ExchangeRepository>(ExchangeRepository);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(repository).toBeDefined();
+    expect(exchangeRepository).toBeDefined();
   });
 
-  // Adicione seus testes específicos aqui
+  describe('createExchange', () => {
+    it('should successfully create and save an exchange', async () => {
+      const exchange: Partial<Exchange> = {
+        id: '1',
+        sourceCurrency: 'USD',
+        targetCurrency: 'EUR',
+        sourceValue: 100,
+        targetValue: 85,
+        userId: 'user1',
+      };
+      mockExchangeRepository.create.mockReturnValue(exchange);
+      mockExchangeRepository.save.mockResolvedValue(exchange);
+
+      const result = await exchangeRepository.createExchange(exchange);
+      expect(mockExchangeRepository.create).toHaveBeenCalledWith(exchange);
+      expect(mockExchangeRepository.save).toHaveBeenCalledWith(exchange);
+      expect(result).toEqual(exchange);
+    });
+  });
+
+  describe('findByUserId', () => {
+    it('should return an array of exchanges with the given userId', async () => {
+      const userId = 'user1';
+      const exchanges: Exchange[] = [
+        {
+          id: '1',
+          transactionId: '02092d27-69f3-4c5f-b16e-19b53d0374a4',
+          rate: 1,
+          datetime: '2023-04-14T00:00:00.000Z',
+          sourceCurrency: 'USD',
+          targetCurrency: 'EUR',
+          sourceValue: 100,
+          targetValue: 85,
+          userId: 'user1',
+        },
+        {
+          id: '2',
+          transactionId: '02092d27-69f3-4c5f-b16e-19b53d037123',
+          rate: 1.2,
+          datetime: '2023-04-14T00:00:00.000Z',
+          sourceCurrency: 'GBP',
+          targetCurrency: 'USD',
+          sourceValue: 50,
+          targetValue: 68,
+          userId: 'user1',
+        },
+      ];
+      mockExchangeRepository.find.mockResolvedValue(exchanges);
+
+      const result = await exchangeRepository.findByUserId(userId);
+      expect(mockExchangeRepository.find).toHaveBeenCalledWith({ where: { userId } });
+      expect(result).toEqual(exchanges);
+    });
+  });
 });
